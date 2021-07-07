dd88ff93aaf54a5d147fdedcd007ced4c80f994b
alexander-lupandin <lupandin.alexander@gmail.com>
2021-07-07 15:28:48 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
-
+<script>
+    alert( 'Привет, мир!' );
+  </script>
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
